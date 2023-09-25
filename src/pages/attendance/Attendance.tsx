2d6ea4e16f07d0ff3bd779f222de891b6daa9090
Ali Soliman <alisolimanworks@gmail.com>
2023-09-25 21:01:30 +0300

    Attendance page initiated

---
+++ src/pages/attendance/Attendance.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+function AttendancePage() {
+  return (
+    <Stack>
+      <Typography>Attendance</Typography>
+    </Stack>
+  );
+}
+
+export default AttendancePage;
